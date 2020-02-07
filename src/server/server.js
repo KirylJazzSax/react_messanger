604a313dae7ed3ead2b4f905ac73c55db860f3ad
KirylJazzSax <matsak_92@mail.ru>
2020-02-07 08:42:12 +0300

    edit port

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const fallback = require ('express-history-api-fallback')
+const PORT = process.env.PORT
 
 const app = express ()
 
@@ -7,6 +8,6 @@ app.use (express.json ())
 app.use ('/', express.static ('dist'))
 app.use (fallback ('index.html', { root: 'dist' }))
 
-app.listen (8080, () => {
+app.listen (PORT, () => {
     console.log ('should work')
 })
